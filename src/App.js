8994ca1297e32d9e418f85c616aa5237bdc23e4f
Satyam-Sharma-coder <138916953+Satyam-Sharma-coder@users.noreply.github.com>
2025-04-11 14:58:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
 import Layout from './Layout';
-import {Routes,Route} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import LoginComponent from './LoginComponent';
 import ForgotPassword from './ForgotPassword';
 import SignUpComponent from './SignUpComponent';
@@ -9,9 +9,8 @@ import SignUpComponent from './SignUpComponent';
 function App() {
   return (
     <div className="App">
-      <p>hi</p>
-      <Layout />
       <Routes> 
+        <Route path="/" element={<Layout />} />
         <Route path="/login" element={<LoginComponent/>}/>
         <Route path="/forgot-password" element={<ForgotPassword/>}/>
         <Route path="/signup" element={<SignUpComponent />}/>
